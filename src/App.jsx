eec271988c6ee08dd5dc28777fea793510a200b2
habibullahjs <limonkhan9020@gmail.com>
2025-07-12 11:11:29 +0600

    limon

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="text-red-500 font-dms">hello</div>
+    </>
+  )
+}
+
+export default App
